fac37fd6290de25eb252c2ee368e64cca1f54f1b
Jessica <jessica.stpierre@hotmail.com>
2020-11-07 19:03:31 -0500

    - Imported a motion/ animation package, started getting the scene ready

---
+++ Assets/Scripts/JessScripts/AISystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISystem : MonoBehaviour
+{
+    public GameObject waypointParent;
+    public GameObject player;
+    public GameObject enemy;
+
+    void Start()
+    {
+        //call the setup AI function
+    }
+
+    public void IdleButton()
+    {
+
+    }
+
+    public void PatrolButton()
+    {
+
+    }
+
+    public void ChaseButton()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
